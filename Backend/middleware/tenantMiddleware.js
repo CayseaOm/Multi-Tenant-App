1970d941f45a74c3a4f39d7dd7f333b81b1cb177
Om K.C <cayseaom@gmail.com>
2025-08-08 12:21:50 +0545

    Frontend initialized

---
+++ Backend/middleware/tenantMiddleware.js
@@ -0,0 +1,14 @@
+// middleware/tenantMiddleware.js
+const Tenant = require("../models/tenant");
+const getTenantDb = require("../utilis/tenantDb");
+
+module.exports = async (req, res, next) => {
+  const tenantId = req.headers["x-tenant-id"];
+  if (!tenantId) return res.status(400).json({ message: "Missing tenant ID" });
+
+  const tenant = await Tenant.findOne({ name: tenantId });
+  if (!tenant) return res.status(404).json({ message: "Tenant not found" });
+
+  req.db = await getTenantDb(tenant.name, tenant.dbUri);
+  next();
+};
